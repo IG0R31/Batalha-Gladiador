6992bfb1481a01a7cdd44e82dde1920f3d16561d
Joao-Betoni <joaobetoni01@gmail.com>
2026-09-12 17:33:47 -0300

    Implementado parâmetros e métodos da classe AtributosBatalha, implementado variável aparencia ao gladiador utilizando biblioteca Random

--- src/main/java/model/AtributosBatalha.java
+++ src/main/java/model/AtributosBatalha.java
@@ -1,6 +1,42 @@
 package model;
+import java.util.Random;
 
 public class AtributosBatalha {
+
+    private double forca;
+    private double agilidade;
+    private double stamina;
+
+    private final double forcaBase =  100;
+    private final double agilidadeBase =  50;
+    private final double staminaBase =  75;
+
+    int randomFactor = 4;
+
+
+    AtributosBatalha(){
+        forca = (forcaBase + ((new Random().nextDouble(50))*randomFactor));
+        agilidade = (agilidadeBase + ((new Random().nextDouble(50))*randomFactor));
+        stamina = (staminaBase + ((new Random().nextDouble(50)*randomFactor)));
+    }
+
+    //getters and setters
+
+    public double getForca(){return forca;}
+    public void setForca(int forca){this.forca = forca;}
+
+    public double getAgilidade(){return agilidade;}
+    public void setAgilidade(int agilidade){this.agilidade = agilidade;}
+
+    public double getStamina(){return stamina;}
+    public void setStamina(){this.stamina = stamina;};
+
+    //getters atributos resultados de razões
+
+    public double getAtaque(){return (10+(forca*0.10));}
+    public double getHealth(){return (500+(3*stamina));}
+    public double getCritico(){return (0.10+(0.02*(agilidade/2)));}
+
 }
 
 
--- src/main/java/model/Gladiador.java
+++ src/main/java/model/Gladiador.java
@@ -1,16 +1,19 @@
 package model;
+import java.util.Random;
 
 public class Gladiador {
     private String nome;
     private String descricao;
     private int batalhasVencidas;
     private StatusGladiador status;
+    private int aparencia;
 
     public Gladiador(String nome, String descricao, int batalhasVencidas, StatusGladiador status) {
         this.nome = nome;
         this.descricao = descricao;
         this.batalhasVencidas = 0;
         this.status = status;
+        aparencia = new Random().nextInt(10); // Numero random de 1 a 10 para definir a aparencia do gladiador
     }
 
     //getters e setters
@@ -25,6 +28,9 @@ public class Gladiador {
 
     public StatusGladiador getStatus(){ return status; }
     public void setStatus(StatusGladiador status){ this.status = status; }
+
+    public int getAparencia(){return aparencia;}
+    public void setAparencia(int aparencia){this.aparencia = aparencia;}
 
     @Override
     public String toString(){
