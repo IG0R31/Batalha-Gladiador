2790495d7bb5a6160c0a7645b35e7d5738cb78cf
Joao-Betoni <joaobetoni01@gmail.com>
2026-09-21 00:05:58 -0300

    implementação pequena dos repositórios na gladiadorService.

--- src/main/java/service/GladiadorService.java
+++ src/main/java/service/GladiadorService.java
@@ -4,12 +4,30 @@ package service;
 import model.Gladiador;
 import model.Usuario;
 import repository.UsuarioRepository;
+import repository.GladiadorRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 
 public class GladiadorService {
 
+    private final UsuarioRepository usuarioRepository;
+    private final GladiadorRepository gladiadorRepository;
+
+    public GladiadorService(
+            UsuarioRepository usuarioRepository,
+            GladiadorRepository gladiadorRepository){
+
+        this.usuarioRepository = usuarioRepository;
+        this.gladiadorRepository = gladiadorRepository;
+    }
+
+
     public Gladiador criarGladiador(Long usuarioId, Gladiador gladiador){
+
+        Optional <Usuario> usuario = usuarioRepository.findById(usuarioId);
+
         return null;
     } // desconta 250 créditos
 
