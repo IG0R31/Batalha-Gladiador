a42f4585087549e3947fa4f7eb4ea9fe11c23295
Igor <igor.oliveiraduarte05@gmail.com>
2026-09-21 21:31:47 -0300

    Arrumando o meu banco de dados, deixando com as configurações corretas. Eu fiz um teste para puxar outra parte do tomcat, mas é mais facil usar mvnw spring-boot:run

--- src/main/java/uel/br/batalha_gladiador/BatalhaGladiadorApplication.java
+++ src/main/java/uel/br/batalha_gladiador/BatalhaGladiadorApplication.java
@@ -2,7 +2,9 @@ package uel.br.batalha_gladiador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // scanBasePackages inclui os pacotes controller e service, que ficam fora
@@ -11,8 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = {"uel.br.batalha_gladiador", "controller", "service"})
 @EntityScan("model")
 @EnableJpaRepositories("repository")
-public class BatalhaGladiadorApplication {
-
+public class BatalhaGladiadorApplication extends SpringBootServletInitializer {
+    //Chamado na execução embutida (mvnw spring-boot:run / run config Spring Boot)
     public static void main(String[] args) {
         SpringApplication.run(BatalhaGladiadorApplication.class, args);
     }
